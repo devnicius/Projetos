5b8987f17f72e38e81533c20c15da300e173980b
Vinícius Silva de Oliveira <devnicius@protonmail.com>
2021-01-06 10:35:29 -0300

    Projeto
    Comitando o projeto Login e Cadastro.

---
+++ Login-e-Cadastro/Script/Script.js
@@ -0,0 +1,12 @@
+function Entrar() {
+    window.alert('Entrou')
+    window.location.href="https://google.com"
+}
+
+function Cadastro() {
+    window.location.href="Cadastro.html"
+}
+
+function Voltar() {
+    window.location.href="Index.html"
+}
