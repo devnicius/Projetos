3b72a8e2193bbe99c7c77c0f55891594f443bb5c
Vinícius Silva de Oliveira <devnicius@protonmail.com>
2021-02-11 10:03:16 -0300

    Update: Layout de site pessoal

---
+++ docs/Template_02/templatemo_531_reflux/assets/js/script.js
@@ -0,0 +1,11 @@
+function showMore(el) {
+
+    document.getElementById(el).style.display = 'block';   
+    document.getElementById(el).style.background = '#a4b6c1';
+}
+
+function hideMore(el) {
+
+    document.getElementById(el).style.display = 'none';
+
+  }
