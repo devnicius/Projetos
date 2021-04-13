4caebd8880944ed80e2d7ab73d29745c20ae0907
Vinícius Silva de Oliveira <devnicius@protonmail.com>
2021-04-13 10:54:41 -0300

    Projects

--- docs/Reviews/app.js
+++ docs/Reviews/app.js
@@ -1,31 +1,77 @@
-﻿//set initial count
-let count = 0;
+﻿//set local reviews data
+const reviews = [
+    {
+    id: 1,
+    name: "Pessoa1",
+    job: "Web Developer",
+    img: "img/pessoa-1.png",
+    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged",
+},
+{
+    id: 2,
+    name: "Pessoa2",
+    job: "Web Designer",
+    img: "img/pessoa-2.png",
+    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+},
+{
+    id: 3,
+    name: "Pessoa3",
+    job: "Full-stack Developer",
+    img: "img/pessoa-3.png",
+    text: "O meu nome é Vinícius, eu tenho 20 anos e é isso! Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+},
+]
 
-//select value and buttons
-const value = document.querySelector('#value');
-const btns = document.querySelectorAll(".btn");
+//select itens
 
-btns.forEach(function (btn) {
-    btn.addEventListener('click', function(e){
-        const styles = e.currentTarget.classList;
-        if (styles.contains('diminuir')){
-            count--;
-        }
+const img = document.getElementById("person-img");
+const author = document.getElementById("name");
+const job = document.getElementById("job");
+const text = document.getElementById("info");
 
-        else if (styles.contains('aumentar')){
-            count++;
-        } else {
-            count = 0;
-        }
-        if(count > 0){
-            value.style.color = "green"
-        }
-        if (count < 0) {
-            value.style.color = "red";
-        }
-        if (count == 0) {
-            value.style.color = "#222";
-        }
-        value.textContent = count;
-    });
+const prevBtn = document.querySelector(".btn.prev");
+const randomBtn = document.querySelector(".btn.random");
+const nextBtn = document.querySelector(".btn.next");
+
+//set starting item
+let currentItem = 2;
+
+// load initial item
+window.addEventListener('DOMContentLoaded', function() {
+    showPerson();
+});
+
+function showPerson(){
+    const item = reviews[currentItem];
+    img.src = item.img; // ou = reviews[currentItem].img
+    author.textContent = item.name;
+    job.textContent = item.job;
+    text.textContent = item.text;
+};
+
+//next person
+nextBtn.addEventListener('click', function(){
+    currentItem++;
+    if(currentItem > reviews.length - 1) {
+        currentItem = 0;
+    }
+    showPerson();
+});
+
+//random person
+randomBtn.addEventListener('click', function(){
+    currentItem = Math.floor(Math.random() * reviews.length);
+    console.log(currentItem);
+    showPerson();
+});
+
+
+//prev person
+prevBtn.addEventListener('click', function(){
+    currentItem--;
+    if (currentItem < 0){
+        currentItem = reviews.length - 1;
+    }
+    showPerson();
 });
